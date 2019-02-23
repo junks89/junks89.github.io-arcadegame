e0df5cdc1454e3260802291329a33de09be7d775
daniel <daniel.unfried@gmail.com>
2019-02-23 07:51:52 +0100

    type:  New Feature
    finished development of the Arcade Game

--- README.md
+++ README.md
@@ -3,18 +3,18 @@
 ## Table of Contents
 
 - [Instructions](#instructions)
-- [Contributing](#contributing)
+- [How to play](#howtoplay)
+- [Project Requirements](#projectRequirements)
 
 ## Instructions
 
-Use this asdada  [rubric](https://review.udacity.com/#!/rubrics/15/view) for self-checking your submission.
+To run the application download the Github Zipfile (or clone the Github repository), extract it and double click on the index.html file within it. 
+There is also a live demo available: [Live Demo](https://junks89.github.io/nanodegree-arcadegame/)
 
-Make sure the functions you write are **object-oriented** - either class functions (like `Player` and `Enemy`) or class prototype functions such as `Enemy.prototype.checkCollisions`. Also make sure that the keyword `this` is used appropriately within your class and class prototype functions to refer to the object the function is called upon.
+## How to play 
 
-Your **README.md** file should be updated with instructions on both how to 1. Run and 2. Play your arcade game.
+By loading the Page the game is started. By pressing "1" you can change the style of your playing character. The goal of the game is to reach the water area on the top of the screen and avoid getting hit by an enemy bug. There are 10 Levels available and more enemy bugs will be added each level. Even the speed of the enemy bugs wiill be increase over time. By using the Arrow Keys your character can move left, right, up and down.
 
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+## Project Requirements
 
-## Contributing
-
-This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
+[link to the Requirements!](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true)
--- js/app.js
+++ js/app.js
@@ -1,45 +1,181 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+class Enemy {
+    constructor(speed, x, y) {
+        this.sprite = 'images/enemy-bug.png';
+        this.speed = speed;
+        this.x = x;
+        this.y = y;
+    };
+    /* update: updates the position of the enemy depending on their speed and position */
+    update(dt) {
+        this.x = this.speed * dt + this.x;
+        this.checkCollision();
+        if (this.x > 505) {
+            this.x = -100;
+        }
+    };
+    /* checkCollision: checks if the enemy is in reach of the player */
+    checkCollision() {
+        if (this.x <= player.x + 50 && this.x >= player.x - 50
+            && this.y <= player.y + 50 && this.y >= player.y - 25) {
+            player.reset(false);
+        }
+    }
+    /* render: Draw the enemy on the screen, required method for game */
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    };
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+class Player {
+    constructor(speed, x, y) {
+        this.speed = speed;
+        this.x = x;
+        this.y = y;
+        this.playerStyleI = 0;
+        this.playerStyles = [
+            'images/char-cat-girl.png',
+            'images/char-horn-girl.png',
+            'images/char-pink-girl.png',
+            'images/char-princess-girl.png',
+            'images/char-boy.png'
+        ];
+        this.sprite = this.playerStyles[this.playerStyleI];
+    };
+    /* update: updates the position of the player depending on their speed and position */
+    update(dt) {
+        if (this.y <= -5) {
+            this.x = 200;
+            this.y = 400;
+            level++;
+            updateLevel();
+            allEnemies = [];
+            if (level <= 10) {
+                for (let i = 1; i <= level; i++) {
+                    let enemy = new Enemy(50 + (i * 20), Math.random() * 150 * (-Math.random()), Math.random() * 240);
+                    allEnemies.push(enemy);
+                }
+            }
+            else {
+                this.reset(true);
+            }
+        }
+    };
+    /* reset: resets the game if the player won or was caught by an enemy */
+    reset(gameWon) {
+        if (gameWon) {
+            let modalText = document.getElementById('modalText');
+            modalText.innerText = `Congratulations you finished all 10 levels and won the game!`;
+            modal.style.display = "block";
+            allEnemies = [];
+            level = 1;
+            updateLevel();
+            this.x = 200;
+            this.y = 400;
+            let enemy = new Enemy(50, 0, 200);
+            allEnemies.push(enemy);
+        }
+        else {
+            allEnemies = [];
+            level = 1;
+            updateLevel();
+            this.x = 200;
+            this.y = 400;
+            let enemy = new Enemy(50, 0, 200);
+            allEnemies.push(enemy);
+        }
+    };
+    /* changePlayerStyle: by hitting "1" the style of the player character changes */
+    changePlayerStyle() {
+        this.playerStyleI++;
+
+        if (this.playerStyleI === this.playerStyles.length) {
+            this.playerStyleI = 0;
+        }
+
+        this.sprite = this.playerStyles[this.playerStyleI];
+    };
+    /* render: Draw the player on the screen, required method for game*/
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    };
+    /* handleInput: handles the action depending on the key pressed */
+    handleInput(keypress) {
+        if (keypress == 'left') {
+
+            if ((this.x - this.speed) >= -15) {
+                this.x = this.x - this.speed;
+            }
+            else {
+                this.x = -15
+            }
+        }
+        if (keypress == 'right') {
+            if ((this.x + this.speed) <= 420) {
+                this.x = this.x + this.speed;
+            }
+            else {
+                this.x = 420;
+            }
+        }
+        if (keypress == 'down') {
+            if ((this.y + this.speed) <= 435) {
+                this.y = this.y + this.speed;
+            }
+            else {
+                this.y = 435;
+            }
+        }
+        if (keypress == 'up') {
+            if ((this.y - this.speed) >= -10) {
+                this.y = this.y - this.speed;
+            }
+            else {
+                this.y = -10;
+            }
+        }
+        if (keypress == '1') {
+            this.changePlayerStyle();
+        }
+    };
+
+};
+/* updateLevel: updates the current level information on the screen */
+function updateLevel(levelElement) {
+    if (!levelElement) {
+        levelElement = document.getElementsByTagName('div')[2];
+    }
+    levelElement.innerHTML = `Level: ${level}`;
+    let canvas = document.getElementsByTagName('canvas')[0];
+    document.body.insertBefore(levelElement, canvas);
 };
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+let levelElement = document.createElement('div');
+let allEnemies = [];
+let level = 1;
+updateLevel(levelElement);
+let player = new Player(20, 200, 400);
+let enemy = new Enemy(50, 0, 200);
+allEnemies.push(enemy);
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+/*source https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_modal*/
+let modal = document.getElementById('myModal');
+let span = document.getElementsByClassName("close")[0];
+span.onclick = function () {
+    modal.style.display = "none";
+}
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
 
-
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-
-
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keydown', function (e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
         39: 'right',
-        40: 'down'
+        40: 'down',
+        49: '1'
     };
 
     player.handleInput(allowedKeys[e.keyCode]);
